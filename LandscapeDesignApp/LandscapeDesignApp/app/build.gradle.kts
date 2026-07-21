29fca99a9f27f77382f494b98b54844d5dd2c218
zenithbabiker-creator <zenithbabiker@gmail.com>
2026-07-21 14:32:40 +0200

    Update build.gradle.kts

--- LandscapeDesignApp/LandscapeDesignApp/app/build.gradle.kts
+++ LandscapeDesignApp/LandscapeDesignApp/app/build.gradle.kts
@@ -45,7 +45,7 @@ android {
 dependencies {
     // ARCore & Huawei AREngine Support
     implementation("com.google.ar:core:1.44.0")
-    implementation("com.huawei.hms:arenginesdk:3.11.0.3")
+    implementation("com.huawei.hms:arenginesdk:3.8.0.300")
 
     // SceneView
     implementation("io.github.sceneview:arsceneview:2.2.1")
